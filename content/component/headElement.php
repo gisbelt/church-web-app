bbaa7203e0316dc06ac81ed6912b0db9833bcfbe
GisbelT <gisbeltq@gmail.com>
2022-09-02 18:54:53 -0400

    ajustes

--- content/component/headElement.php
+++ content/component/headElement.php
@@ -9,8 +9,12 @@ class headElement{
             ' <!-- Required meta tags -->
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            <meta name="author" content="Grupo 2">
-            <meta name="description" content="Proyecto Uptaeb">
+            <meta name="author" content="Uptaeb">
+            <meta name="copyright" content="Uptaeb 2022"/>
+            <meta name="description" content="Aplicación Web Iglesia Evangélica La Gracia de Dios">
+            <meta name="theme-color" content="#0191b3" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="#0191b3" />
+            <link rel="icon" href="../assets/img/logo.png">
 
             <!-- Bootstrap CSS v5.0.2 -->
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
