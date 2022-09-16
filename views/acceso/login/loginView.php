ed1f336336ad6634c6c8b447b459e39db70f3834
GisbelT <gisbeltq@gmail.com>
2022-09-15 22:59:29 -0400

    ajustes

--- views/acceso/login/loginView.php
+++ views/acceso/login/loginView.php
@@ -28,15 +28,14 @@ $this->title = 'Home'
                 <!-- Creamos Formulario: !crt-form-login -->
                 <!-- Enviamos los datos del formulario a través del método post -->
                 <form method="post" action="/login" id="login-form" name="login-form">
-                    <div class="form-group p-1">
-                        <input type="text" class="form-control form-input mb-2" name="email" id="email"
-                               aria-describedby="emailHelp" placeholder=" " value="admin@gmail.com" autocomplete="off">
-                        <label for="email" class="form-label fw-bold mx-auto">Correo:</label>
+                    <div class="form-group">
+                        <input type="text" class="form-control form-input mb-2" name="email" id="email" aria-describedby="emailHelp" placeholder=" " value="admin@gmail.com" autocomplete="off">
+                        <label for="email" class="form-label fw-bold">Correo:</label>
                     </div>
-                    <div class="form-group p-1">
-                        <input type="password" class="form-control form-input mb-2" name="password" id="password"
-                               placeholder=" " value="123456" autocomplete="off">
-                        <label for="password" class="form-label fw-bold  mx-auto">Contraseña:</label>
+                    <br>
+                    <div class="form-group">
+                        <input type="password" class="form-control form-input mb-2" name="password" id="password" placeholder=" " value="123456" autocomplete="off">
+                        <label for="password" class="form-label fw-bold">Contraseña:</label>
                     </div>
                     <button type="submit" id="login" name="login" class="btn btn-primary w-100 mt-3">Iniciar Sesión
                     </button>
