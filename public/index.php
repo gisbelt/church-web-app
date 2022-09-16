203065a3ecdf77f6538f52ec52cfafeff6bfb8a9
Carlos Hurtado <hurtadocarlo96@ggmail.com>
2022-09-16 19:28:07 -0400

    ajustes de datos

--- public/index.php
+++ public/index.php
@@ -82,6 +82,7 @@ use Monolog\Logger;
 
 session_start();
 $globalConfig = new sysConfig();
+$globalConfig->_int();
 $app = new Aplicacion(dirname(__DIR__));
 $rutas = rutas();
 foreach ($rutas as $key => $ruta) {
