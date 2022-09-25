0b06fba52dd0e0b1db38ecce35550262301bfaa9
Yeltsin Linares <yeltsin207@gmail.com>
2022-09-25 00:09:01 -0400

    actividades

--- content/collections/actividadesCollection.php
+++ content/collections/actividadesCollection.php
@@ -2,6 +2,7 @@
     
     namespace content\collections;
     
+    use DateTime;
     use Monolog\Handler\StreamHandler;
     use Monolog\Logger;
 
@@ -10,24 +11,26 @@
         public function formatActividades($actividades)
         {
             $data = [];
-            $logger = new Logger("web");
-            $logger->pushHandler(new StreamHandler(__DIR__ . "./../../Logger/log.txt", Logger::DEBUG));
-            
             foreach ($actividades as $actividad) {
                 $actividad['actions'] = sprintf(
-                    '<a href="%s" class="btn btn-info me-2" target="_blank"><i class="bi bi-pencil text-light"></i></a>',
+                    '<a href="" class="btn btn-info me-2" target="_blank"><i class="bi bi-pencil text-light"></i></a>',
                 );
-                $logger->debug(__METHOD__, [$actividad]);
                 $actividad['actions'] .= sprintf(
-                    '<button type="button" data-route="%s" name="eliminar-donacion" id="eliminar-donacion" class="btn btn-danger ms-2"><i class="bi bi-trash text-light"></i>
+                    '<button type="button"  name="eliminar-donacion" id="eliminar-donacion" class="btn btn-danger ms-2"><i class="bi bi-trash text-light"></i>
                           </button>',
                 );
+                $date = $actividad['fecha'].' '.$actividad['hora'];
+                $date = new DateTime($date);
+                $actividad['fecha'] = $date->format('d/m/Y H:i:s');
+                $logger = new Logger("web");
+                $logger->pushHandler(new StreamHandler(__DIR__ . "./../../Logger/log.txt", Logger::DEBUG));
+                $logger->debug(__METHOD__, [$actividad['fecha']]);
+                
                 $data[] = $actividad;
-                $logger->debug(__METHOD__, [$data]);
             }
             
           
-            $logger->debug(__METHOD__, [$data]);
+            
             return $data;
         }
     }
