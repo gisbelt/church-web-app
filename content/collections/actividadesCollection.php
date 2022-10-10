390055def038c6eb20f73c02f369bc96e982b756
Yeltsin Linares <yeltsin207@gmail.com>
2022-10-09 23:39:10 -0400

    editar eactividades

--- content/collections/actividadesCollection.php
+++ content/collections/actividadesCollection.php
@@ -2,6 +2,7 @@
     
     namespace content\collections;
     
+    use content\enums\estadosActividad as status;
     use DateTime;
     use Monolog\Handler\StreamHandler;
     use Monolog\Logger;
@@ -12,6 +13,23 @@
         {
             $data = [];
             foreach ($actividades as $actividad) {
+                switch ($actividad['status']){
+                    case status::$en_curso:
+                        $actividad['status'] = 'En Curso';
+                        break;
+                    case status::$en_pausa:
+                        $actividad['status'] = 'En Pausa';
+                        break;
+                    case status::$terminado:
+                        $actividad['status'] = 'Terminado';
+                        break;
+                    case status::$cancelado:
+                        $actividad['status'] = 'Cancelado';
+                        break;
+                    default:{
+                        $actividad['status'] = 'No Disponible';
+                    }
+                }
                 $actividad['actions'] = sprintf(
                     '<a href="%s" class="btn btn-info me-2" target="_blank"><i class="bi bi-pencil text-light"></i></a>',
                     '/actividades/editar/' . $actividad['id']
