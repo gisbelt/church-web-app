f4a092dc2a8b17fa6d253cc57322f8d4e4fa9157
Carlos Hurtado <hurtadocarlo96@ggmail.com>
2022-10-10 23:13:17 -0400

    ajustes

--- content/models/bitacoraModel.php
+++ content/models/bitacoraModel.php
@@ -24,4 +24,28 @@ class bitacoraModel extends  BD
         VALUES (?,?,?,?)");
         return $sql->execute([$descripcion, $modulo, $_SESSION['user'], $fecha]);
     }
+
+    // Consultar registro de bitacora
+    public static function consultar($user, $fechaInicial, $fechaFinal)
+    {
+        $connexionBD = BD::crearInstancia();
+        $query = "SELECT bitacora.id, bitacora.fecha_creado AS fecha, bitacora.descripcion, bitacora.modulo, usuarios.username, usuarios.id as user FROM bitacora INNER JOIN usuarios ON usuarios.id = bitacora.user_id";
+
+        $conditions = array();
+        if (!is_null($user)) {
+            $conditions[] = "usuarios.id='$user'";
+        }
+        if (!is_null($fechaInicial) && !is_null($fechaFinal)) {
+            $conditions[] = "bitacora.fecha_creado BETWEEN '$fechaInicial' AND '$fechaFinal'";
+        }
+
+        $queryString = $query;
+        if (count($conditions) > 0) {
+            $queryString .= " WHERE " . implode(' AND ', $conditions);
+        }
+        $sql = $connexionBD->prepare($queryString);
+        $sql->execute();
+        $bitacora = $sql->fetchAll(PDO::FETCH_ASSOC);
+        return $bitacora;
+    }
 }
